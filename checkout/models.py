226ce6517c9a0018ba6278bf4d67c75dfa2247c6
maxapn98 <Nikolamerano@gmail.com>
2022-04-13 22:10:14 +0000

    Add stripe succsess and web_hook heandler

--- checkout/models.py
+++ checkout/models.py
@@ -6,9 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-class Topic(models.Model):
-    id = models.AutoField(primary_key=True)
-
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -37,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
